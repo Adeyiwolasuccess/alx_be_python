2111ff10f8cb24b7804a6d07aeb0f39ca58c5e8e
adeyiwola success <successadeyiwola@gmail.com>
2025-05-22 19:25:36 -0700

    Add basic_operations.py to python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# Assign specific values to two variables
+number1 = 10
+number2 = 5
+
+# Perform addition
+sum_result = number1 + number2
+
+# Perform subtraction
+difference_result = number1 - number2
+
+# Perform multiplication
+product_result = number1 * number2
+
+# Print the results of these operations
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
