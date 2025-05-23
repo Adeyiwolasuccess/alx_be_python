1412e133f379806d81ce4208933e6d09f3eaf814
adeyiwola success <successadeyiwola@gmail.com>
2025-05-22 18:42:18 -0700

    Add basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Performing calculations:
+
+number1 = 10
+number2 = 5
+
+# perform the addition, subtraction and the multiplication:
+
+Addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+print("The Addition of", number1, "and", number2, "is", Addition)
+print("The subtraction of", number1,"and", number2, "is", subtraction)
+print("The multiplication of", number1, "and", number2, "is", multiplication)
